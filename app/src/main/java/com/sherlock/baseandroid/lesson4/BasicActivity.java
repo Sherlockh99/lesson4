e97a0cadab65fe165e6c6d41fe3f7774dcb6d862
Sherlockh99 <sherlockh989@gmail.com>
2022-11-21 22:01:17 +0200

    Part 2.11

---
+++ app/src/main/java/com/sherlock/baseandroid/lesson4/BasicActivity.java
@@ -0,0 +1,59 @@
+package com.sherlock.baseandroid.lesson4;
+
+
+import android.os.Bundle;
+import android.content.SharedPreferences;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasicActivity extends AppCompatActivity {
+    static final String NameSharedPreference = "LOGIN";
+    final String appTheme = "APP_THEME";
+    final int MyCoolCodeStyle = 0;
+    final int AppThemeLightCodeStyle = 1;
+    final int AppThemeCodeStyle = 2;
+    final int AppThemeDarkCodeStyle = 3;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(getAppTheme(R.style.MyColorStyle));
+    }
+
+    private int getAppTheme(int codeStyle){
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+
+    int codeStyleToStyleId(int codeStyle){
+        switch (codeStyle){
+            case AppThemeCodeStyle:
+                return R.style.AppTheme;
+            case AppThemeLightCodeStyle:
+                return R.style.AppThemeLight;
+            case AppThemeDarkCodeStyle:
+                return R.style.AppThemeDark;
+            default:
+                return R.style.MyColorStyle;
+        }
+    }
+
+    //Чтение настроек, параметр Тема
+    int getCodeStyle(int codeStyle){
+        //Работаем через специальный класс сохранения и чтения настроек
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,MODE_PRIVATE);
+        //прочитать тему, если настройка не найдена - взять по умолчанию
+        return sharedPref.getInt(appTheme, codeStyle);
+    }
+
+    //сохранение настроек
+    void setAppTheme(int codeStyle){
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,MODE_PRIVATE);
+        //настройки сохраняются посредством специального класса editor
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(appTheme,codeStyle);
+        editor.apply();
+    }
+
+}
--- app/src/main/java/com/sherlock/baseandroid/lesson4/StylesActivity.java
+++ app/src/main/java/com/sherlock/baseandroid/lesson4/StylesActivity.java
@@ -1,30 +1,18 @@
 package com.sherlock.baseandroid.lesson4;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.radiobutton.MaterialRadioButton;
 
-public class StylesActivity extends AppCompatActivity {
-
-    private static final String NameSharedPreference = "LOGIN";
-    private static final String appTheme = "APP_THEME";
-    private static final int MyCoolCodeStyle = 0;
-    private static final int AppThemeLightCodeStyle = 1;
-    private static final int AppThemeCodeStyle = 2;
-    private static final int AppThemeDarkCodeStyle = 3;
-
+public class StylesActivity extends BasicActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(getAppTheme(R.style.MyColorStyle));
         setContentView(R.layout.activity_styles);
-
         initThemeChooser();
     }
 
@@ -48,37 +36,4 @@ public class StylesActivity extends AppCompatActivity {
         });
     }
 
-    private int getAppTheme(int codeStyle){
-        return codeStyleToStyleId(getCodeStyle(codeStyle));
-    }
-
-    //Чтение настроек, параметр Тема
-    private int getCodeStyle(int codeStyle){
-        //Работаем через специальный класс сохранения и чтения настроек
-        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,MODE_PRIVATE);
-        //прочитать тему, если настройка не найдена - взять по умолчанию
-        return sharedPref.getInt(appTheme, codeStyle);
-    }
-
-    //сохранение настроек
-    private void setAppTheme(int codeStyle){
-        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,MODE_PRIVATE);
-        //настройки сохраняются посредством специального класса editor
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(appTheme,codeStyle);
-        editor.apply();
-    }
-
-    private int codeStyleToStyleId(int codeStyle){
-        switch (codeStyle){
-            case AppThemeCodeStyle:
-                return R.style.AppTheme;
-            case AppThemeLightCodeStyle:
-                return R.style.AppThemeLight;
-            case AppThemeDarkCodeStyle:
-                return R.style.AppThemeDark;
-            default:
-                return R.style.MyColorStyle;
-        }
-    }
 }
